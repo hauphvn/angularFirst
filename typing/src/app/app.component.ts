c76366d85d922ae8b2f734bd37d7d17bb4ad9385
tonyc <hauphvn@gmail.com>
2021-07-03 11:57:15 +0700

    demo typing, install faker text random

---
+++ typing/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {lorem} from 'faker';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  randomText = lorem.sentence();
+  inputText = '';
+
+  onInput(value: string) {
+    this.inputText = value;
+  }
+
+  addClass(letter: string, inputTextElement: string): string {
+    if(!inputTextElement){
+      return "pending";
+    }
+    return (letter === inputTextElement) ? 'correct' : 'incorrect';
+  }
+}
