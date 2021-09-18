d78ee22d4b7bbb180f3593d448df119370b88cab
hauphvn <pvhau@cmc.com.vn>
2021-09-18 10:43:10 +0700

    -ADD_NEW: edit recipe/add new recipe

--- kayFoodBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ kayFoodBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {RecipeModel} from "../../shared/models/recipe.model";
 import {RecipeService} from "../recipe.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,10 +10,16 @@ import {RecipeService} from "../recipe.service";
 })
 export class RecipeListComponent implements OnInit {
   recipes: RecipeModel[] = [];
-  constructor(private recipeService: RecipeService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
   }
 
+  onCreateNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
 }
