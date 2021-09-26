cbf669c392dc1f817b6551c06c019311ad46702e
hauphvn <pvhau@cmc.com.vn>
2021-09-26 11:36:34 +0700

    Using observable in shopping list service

--- kayFoodBook/src/app/shopping-list/shopping-list.component.ts
+++ kayFoodBook/src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IngredientModel} from "../shared/models/ingredient.model";
 import {ShoppingListService} from "./shopping-list.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: IngredientModel[] = [];
-
+  subs: Subscription = new Subscription();
   constructor(private shoppingListService: ShoppingListService) {
   }
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientChanged.subscribe((ingredients: IngredientModel[]) => {
+    this.subs = this.shoppingListService.ingredientChanged.subscribe((ingredients: IngredientModel[]) => {
       if (ingredients) {
         this.ingredients = ingredients;
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
--- kayFoodBook/src/app/shopping-list/shopping-list.service.ts
+++ kayFoodBook/src/app/shopping-list/shopping-list.service.ts
@@ -1,23 +1,24 @@
 import {IngredientModel} from "../shared/models/ingredient.model";
 import {EventEmitter, Injectable} from "@angular/core";
+import {Subject} from "rxjs";
 
 export class ShoppingListService {
   private ingredients: IngredientModel[] = [
     new IngredientModel('Com trang', 5),
     new IngredientModel('Ga ta', 1)
   ];
-  ingredientChanged = new EventEmitter<IngredientModel[]>();
+  ingredientChanged: Subject<IngredientModel[]> = new Subject<IngredientModel[]>();
   getIngredients(): IngredientModel[] {
     return this.ingredients.slice();
   }
 
   addNewIngredient(ingredient: IngredientModel) {
     this.ingredients.push(ingredient);
-    this.ingredientChanged.emit(this.ingredients.slice());
+    this.ingredientChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: IngredientModel[]){
     this.ingredients.push(...ingredients);
-    this.ingredientChanged.emit(this.ingredients.slice());
+    this.ingredientChanged.next(this.ingredients.slice());
   }
 }
